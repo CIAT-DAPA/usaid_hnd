4a7a49bc23c75d0bb5f7e7f3f5446c486c349bea
lizllanos <llanoslizeth@gmail.com>
2017-04-18 15:54:59 -0500

    Add script to a general function

--- 07_organizar_series_raw_hnd.R
+++ 07_organizar_series_raw_hnd.R
@@ -4,8 +4,87 @@
 ## March 2017
 #######################################################
 
-#Monthly data
-rutOrigen="X:/Water_Planning_System/01_weather_stations/hnd_dgrh/monthly_raw/prec-per-station" #Ruta donde se encuentran los archivos .txt
+
+###################################
+#---------Daily data--------------#
+###################################
+variable = c("prec","tmax","tmin")
+
+rutOrigen = paste0("X:/Water_Planning_System/01_weather_stations/hnd_all/daily_raw/",variable,"-per-station/") #Ruta donde se encuentran los archivos .txt
+outDir ="X:/Water_Planning_System/01_weather_stations/hnd_all/" 
+rutCat = "X:/Water_Planning_System/01_weather_stations/catalog_daily.csv"
+
+time_period=seq(as.Date("1980/1/1"), as.Date("2016/12/31"), "days")
+
+organize_data = function(rutOrigen,outDir,rutCat,variable,time_period){
+  dir.create(paste0(outDir,"daily_processed"),showWarnings = F)
+  
+  files <-list.files(rutOrigen,pattern="\\.txt$")
+  nom.files<-substring(files,1,nchar(files)-13)
+  
+  idstation = read.csv(rutCat,header=T) #Cargar base con código y nombre de la estación
+  idstation = idstation[which(idstation$variable==variable),]
+  
+  cod=as.character(idstation$national_code)
+  # not_f = which(is.na(cod))
+  # cod = cod[-not_f]
+  
+  where <- match(cod,nom.files)
+  station_find=nom.files[where[which(!is.na(where))]]
+  station_find_n=paste0(cod[which(!is.na(where))],"_",idstation[which(!is.na(where)),"name_station"])
+  
+   #Definir periodo que se desea analizar
+  
+  fechas=format(time_period,"%Y%m%d")
+  fechas=cbind.data.frame("Date"=fechas,"NA")
+  
+  Datos <- lapply(paste(rutOrigen,cod[which(!is.na(where))],"_raw_",variable,".txt",sep=""),function(x){read.table(x,header=T,sep="\t")})
+  
+  datos_to=as.data.frame(matrix(NA,nrow(fechas),length(Datos)))
+  
+  
+  
+  for(j in 1:length(Datos)) {  
+    
+    old=na.omit(Datos[[j]])
+    if(nrow(old)!=0){
+      combnew=old[!duplicated(old[,1]),]
+    }else{
+      combnew = old
+    }
+    
+    
+    final=merge(fechas,combnew,by="Date",all.x=T)
+    #if(nrow(final)==nrow(datosprecip)){
+    datos_to[,j]=final[,3]
+    #}
+    
+  }
+  
+  year=as.numeric(substr(fechas[,1],1,4))
+  month=as.numeric(substr(fechas[,1],5,6))
+  day=as.numeric(substr(fechas[,1],7,8))
+  
+  datos_fin=cbind(day,month,year,datos_to)
+  
+  names(datos_fin)=c("day","month","year",as.character(station_find_n))
+  
+  
+  #Se guardan los archivos en formato .csv con la info organizada
+  write.csv(datos_fin,paste(outDir,"/daily_processed/",variable,"_daily_raw.csv",sep=""),row.names=F)
+  
+}
+
+for(j in 1:length(variable)) organize_data(rutOrigen,outDir,rutCat,variable[j],time_period)
+
+
+
+
+##################################
+#-------Monthly data-------------#
+##################################
+
+rutOrigen="X:/Water_Planning_System/01_weather_stations/hnd_dgrh/monthly_raw/tmax-per-station" #Ruta donde se encuentran los archivos .txt
 dir.create(paste0(outDir,"monthly_processed"),showWarnings = F)
 
 
@@ -24,67 +103,7 @@ x=seq(as.Date("1980/1/1"), as.Date("2015/12/31"), "month") #Definir periodo que 
 fechas=format(x,"%Y%m")
 fechas=cbind.data.frame("Date"=fechas,"NA")
 
-Datos <- lapply(paste(rutOrigen,"/",station_find,"_raw_prec.txt",sep=""),function(x){read.table(x,header=T,sep=" ")})
-
-Rain = Datos
-datosprecip=as.data.frame(matrix(NA,nrow(fechas),length(Rain)))
-
-
-
-for(j in 1:length(Rain)) {  
-  
-  old=na.omit(Rain[[j]])
-  if(nrow(old)!=0){
-    combnew=old[!duplicated(old[,1]),]
-  }else{
-    combnew = old
-  }
-  
-  
-  final=merge(fechas,combnew,by="Date",all.x=T)
-  #if(nrow(final)==nrow(datosprecip)){
-    datosprecip[,j]=final[,3]
-  #}
-  
-}
-
-year=as.numeric(substr(fechas[,1],1,4))
-month=as.numeric(substr(fechas[,1],5,6))
-#day=as.numeric(substr(fechas[,1],7,8))
-
-precipfin=cbind(month,year,datosprecip)
-
-names(precipfin)=c("month","year",as.character(station_find_n1))
-
-
-#Se guardan los archivos en formato .csv con la info organizada
-write.csv(precipfin,paste("hnd_precip_all.csv",sep=""),row.names=F)
-
-
-
-#Daily data
-rutOrigen = "X:/Water_Planning_System/01_weather_stations/hnd_dgrh/daily_raw_org/prec-per-station/"
-outDir ="X:/Water_Planning_System/01_weather_stations/hnd_dgrh/daily_processed/prec-per-station/" #Ruta donde se encuentran los archivos .txt
-dir.create(paste0(outDir,"daily_processed"),showWarnings = F)
-
-
-files <-list.files(rutOrigen,pattern="\\.txt$")
-nom.files<-substring(files,1,nchar(files)-13)
-
-idstation=read.csv("X:/Water_Planning_System/01_weather_stations/hnd_dgrh/daily_raw/catalog_daily_dgrh.csv",header=T) #Cargar base con código y nombre de la estación
-cod=as.character(idstation$national_code)
-not_f = which(is.na(cod))
-cod = cod[-not_f]
-
-where <- match( cod,nom.files)
-station_find_n=paste0(cod,"_",idstation[-not_f,1])
-
-x=seq(as.Date("1980/1/1"), as.Date("2016/12/31"), "days") #Definir periodo que se desea analizar
-
-fechas=format(x,"%Y%m%d")
-fechas=cbind.data.frame("Date"=fechas,"NA")
-
-Datos <- lapply(paste(rutOrigen,cod,"_prec_raw.txt",sep=""),function(x){read.table(x,header=T,sep="\t")})
+Datos <- lapply(paste(rutOrigen,"/",station_find,"_raw_tmax.txt",sep=""),function(x){read.table(x,header=T,sep=" ")})
 
 Rain = Datos
 datosprecip=as.data.frame(matrix(NA,nrow(fechas),length(Rain)))
@@ -110,12 +129,14 @@ for(j in 1:length(Rain)) {
 
 year=as.numeric(substr(fechas[,1],1,4))
 month=as.numeric(substr(fechas[,1],5,6))
-day=as.numeric(substr(fechas[,1],7,8))
+#day=as.numeric(substr(fechas[,1],7,8))
 
-precipfin=cbind(day,month,year,datosprecip)
+precipfin=cbind(month,year,datosprecip)
 
-names(precipfin)=c("day","month","year",as.character(station_find_n))
+names(precipfin)=c("month","year",as.character(station_find_n1))
 
 
 #Se guardan los archivos en formato .csv con la info organizada
-write.csv(precipfin,paste("X:/Water_Planning_System/01_weather_stations/hnd_dgrh/daily_processed/prec_daily_raw.csv",sep=""),row.names=F)
+write.csv(precipfin,paste("hnd_precip_all.csv",sep=""),row.names=F)
+
+
--- 09_qc_daily.R
+++ 09_qc_daily.R
@@ -7,7 +7,7 @@ inDir = "X:/Water_Planning_System/01_weather_stations/hnd_dgrh/daily_processed/"
 outDir = "X:/Water_Planning_System/01_weather_stations/hnd_dgrh/daily_processed/"
 
 # Define variable
-variable = "prec"
+variable = "tmax"
 
 # Load data base with all raw stations
 data_station = read.csv(paste0(inDir,variable,"_daily_raw.csv"),header = T)
@@ -21,10 +21,8 @@ name_st = paste0(nomb_s[,2]," (",nomb_s[,1],")")
 dates=seq(as.Date("1980/1/1"), as.Date("2016/12/31"), "days") 
 
 
-# Define range of variable
-minim = 0
-maxim = 300
-ric = 10
+# Define values for quality control
+minim = 0 ;maxim = 50; ric = 7; criterio1=15
 
 
 # Function for legend outside plot
@@ -158,6 +156,138 @@ if(variable=="prec"){
   names(object)[-3:-1] = nomb
   write.csv(object,paste0(outDir,"prec_daily_qc.csv"),row.names = F,quote = F)
    
+}
+
+if(variable=="tmax"){
+  dir.create(paste0(outDir,"quality_control"),showWarnings=F)
+  dir.create(paste0(outDir,"quality_control/tmax"),showWarnings=F)
+  
+  for(i in 1:(ncol(object)-3))
+  {
+    cat(paste0("quality_control para la estación "),name_st[i],"\n")
+    
+    ##QC filtro grueso definido por el usuario
+    out_range = which(object[,i+3] < minim | object[,i+3] >maxim)
+    if(length(out_range)>0){object[out_range,i+3]<-NA}
+    
+    ##QC filtro consistencia interna tmax<tmin
+    # out_int = which(tmax[i+3]<tmin[i+3])
+    # object[out_int,i+3]<-NA
+    # 
+    na = sum(is.na(object[,i+3]))/length(object[,i+3])    
+    
+    if(na<0.95){
+      
+      ##QC datos atípicos con RIC
+      x = object[,i+3]
+      year = object$year
+      month.abb.s = c("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dec")
+      month = month.abb.s[as.numeric(object$month)]
+      month = factor(month, levels=month.abb.s)
+     
+     
+      
+      val.na = boxplot(x~month,range=10,plot=F)
+      lim_inf.na = c()
+      lim_sup.na = c()
+      for(j in 1:12){
+        lim_inf.na[month==month.abb[j]]=val.na$stats[1,j]
+        lim_sup.na[month==month.abb[j]]=val.na$stats[5,j]
+        
+      }
+      
+      out_atip.na = which(x > lim_sup.na | x < lim_inf.na)
+      
+      val.na.y = boxplot(x~year,range=10,plot=F)
+      year.nam = as.numeric(val.na.y$names)
+      
+      
+      lim_inf.na.y = c()
+      lim_sup.na.y = c()
+      for(j in 1:length(year.nam)){
+        lim_inf.na.y[year==year.nam[j]]=val.na.y$stats[1,j]
+        lim_sup.na.y[year==year.nam[j]]=val.na.y$stats[5,j]
+        
+      }
+      
+      out_atip.na.y = which(x > lim_sup.na.y | x < lim_inf.na.y)
+      
+      dir.create(paste0(outDir,"quality_control/tmax/boxplot_graph"),showWarnings = F)
+      
+      tiff(paste0(outDir,"quality_control/tmax/boxplot_graph/",nomb[i],"_",variable,"_box_graph_qc.tiff"),compression = 'lzw',height = 7,width = 10,units="in", res=150)
+      par(mfrow = c(1,2),oma = c(0, 0, 2, 0),las=1)
+      boxplot(x~year,range=10,xlab = "Temperatura máxima (°C)",horizontal=T,cex.axis=0.8,main = "Distribución anual")
+      boxplot(x~month,range=10,xlab = "Temperatura máxima (°C)",horizontal=T,cex.axis=0.8,main = "Distribución mensual")
+      title(name_st[i],outer = T)
+      dev.off()
+      
+      object[c(out_atip.na,out_atip.na.y),i+3]<-NA
+      
+      
+      
+      ##QC saltos mayor a umbral definido por el usuario
+      out_salt = which(abs(diff(object[,i+3]))>=as.numeric(criterio1))
+      out_salt.n = unique(sort(c(out_salt,out_salt+1)))
+      object[out_salt.n,i+3]<-NA
+      
+      ##QC datos consecutivos 
+      xc <-data.frame(hasta=cumsum(rle(object[,i+3])$lengths),cant_iguales=rle(object[,i+3])$lengths,valor=rle(object[,i+3])$values)
+      xc$desde = xc$hasta -xc$cant_iguales + 1  
+      xc <-na.omit(xc)
+       
+      out_cons = c()
+      error = xc[xc$cant_iguales>3,] 
+      if (nrow(error)>0){
+        for (k in 1:nrow(error)) {
+          if (k==1){out_cons<-error$desde[k]:error$hasta[k]}
+          else{out_cons<-c(out_cons,error$desde[k]:error$hasta[k])}
+        }
+      }
+      
+      if(length(out_cons)>0){object[out_cons,i+3]<-NA}
+      
+      #######Graficos quality_control###########
+      
+      dir.create(paste0(outDir,"quality_control/tmax/line_graph"),showWarnings = F)
+      tiff(paste0(outDir,"quality_control/tmax/line_graph/",nomb[i],"_",variable,"_line_graph_qc.tiff"),compression = 'lzw',height = 7,width = 16,units="in", res=150)
+      
+      par(mfrow = c(2,1),     # 2x2 layout
+          oma = c(2, 2, 0, 0), # two rows of text at the outer left and bottom margin
+          mar = c(4, 4, 3, 3), # space for one row of text at ticks and to separate plots
+          xpd = NA) 
+      
+      ylim1 = c(min(data_station[,i+3],na.rm=T) ,max(data_station[,i+3],na.rm=T)+1)
+      ylim2 = c(min(object[,i+3],na.rm=T) ,max(object[,i+3],na.rm=T)+1)
+      
+      plot(data_station[,i+3],type="l",col="grey",xaxt="n",yaxt="n",xlab="",ylab="Temperatura máxima (°C)",main = "Antes del control de calidad",ylim=ylim1,cex.lab=0.8)
+      
+      mtext(paste0("Estación ",name_st[i]),side=3,outer=T,adj=0,line=-1.3,cex = 1.2) 
+      points(out_range,data_station[out_range,i+3],col="black",bg="green",cex=0.7,pch=21)
+      points(out_cons,data_station[out_cons,i+3],col="black",bg="red",cex=0.7,pch=21)
+      points(c(out_atip.na.y,out_atip.na),data_station[c(out_atip.na.y,out_atip.na),i+3],col="black",bg="purple",cex=0.7,pch=21)
+      points(out_salt.n,data_station[out_salt.n,i+3],col="black",bg="turquoise1",cex=0.7,pch=24)
+      #points(out_int,tmax[out_int,i+3],col="black",bg="tan3",cex=0.7,pch=21)
+      
+      axis(1,at=seq(1,nrow(data_station),366),labels=seq(min(data_station$year),max(data_station$year),1),las=1,col="black",las =2,cex.axis = 0.8)
+      axis(2,at=seq(ylim1[1],ylim1[2],10),labels=seq(ylim1[1],ylim1[2],10),las=1,col="black",las =1,cex.axis = 0.8)
+      
+      
+      plot(object[,i+3],type="l",col="grey",xaxt="n",yaxt="n",xlab="",ylab="Temperatura máxima (°C)",main = "Después del control de calidad",ylim=ylim2,cex.lab=0.8)
+      
+      axis(1,at=seq(1,nrow(data_station),366),labels=seq(min(data_station$year),max(data_station$year),1),las=1,col="black",las =2,cex.axis = 0.8)
+      axis(2,at=seq(ylim2[1],ylim2[2],5),labels=seq(ylim2[1],ylim2[2],5),las=1,col="black",las =1,cex.axis = 0.8)
+      
+      add_legend("bottomright", legend=c("Datos consecutivos","Datos atípicos","Datos por fuera del rango","Saltos consecutivos"), pch=c(21,21,21,24), 
+                 pt.bg= c("red","purple","green","turquoise1","tan3"),bty = "n",cex = 0.9,
+                 horiz=TRUE)
+      
+      dev.off()
+    }
+    
+  }
+  names(object)[-3:-1] = nomb
+  write.csv(object,paste0(outDir,"tmax_daily_qc.csv"),row.names = F,quote = F)
+  
 }
 
 
