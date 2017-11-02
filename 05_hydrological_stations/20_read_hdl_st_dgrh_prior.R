76f2b63a47fab829343be4fa14e502fb188c1585
lizllanos <llanoslizeth@gmail.com>
2017-11-02 13:41:20 -0500

    Add changes in scripts for new hdl info

--- 05_hydrological_stations/20_read_hdl_st_dgrh_prior.R
+++ 05_hydrological_stations/20_read_hdl_st_dgrh_prior.R
@@ -6,7 +6,7 @@ library(XLConnect)
 # Note: There are some stations with wrong header. We use the previous 
 #       function to correct the header and then run this new function!
 iDir <- "C:/Users/lllanos/Desktop/Prioritarios"
-oDir <- "C:/Users/lllanos/Desktop/Prioritarios"
+oDir <- "C:/Users/lllanos/Desktop"
 
 xlsF_folder <- list.files(iDir, full.names = T)[-2]
 xlsF_all <- sapply(xlsF_folder, list.files, full.names = T)
@@ -80,4 +80,80 @@ readDGRH_n <- function(all_files, st_name,iDir="", xlsF="", oDir=""){
 
 ### Run function
 lapply(1:length(xlsF_all),function(j) readDGRH_n(all_files = xlsF_all[[j]],st_name = st_name[j],iDir, xlsF, oDir))
+
+
+
+# Other format ------------------------------------------------------------
+iDir <- "C:/Users/lllanos/Desktop/Prioritarios"
+oDir <- "C:/Users/lllanos/Desktop"
+
+xlsF_folder <- list.files(iDir, full.names = T)[-2]
+xlsF_all <- sapply(xlsF_folder, list.files, full.names = T)
+st_name = basename(xlsF_folder)
+readDGRH_n <- function(all_files, st_name,iDir="", xlsF="", oDir=""){
+  cat(st_name)
+ # for (i in (length(all_files)-1):length(all_files)){
+    tryCatch({  
+      #cat(".> Read Sheet", i , "/", nsheets, "\n")
+      i=length(all_files)-1
+      xlsF <- loadWorkbook(paste0( all_files[i]))
+      
+      for(sheet in 1:12){
+        cat(sheet)
+        xlsF_s <- readWorksheet(xlsF, sheet =sheet, header = T, startRow = 1, startCol = 1)
+        data <- xlsF_s[!is.na(xlsF_s[,1]),]
+        if(sheet==1){
+           data_n <- t(data[nrow(data)-2,])[-1]
+        }else{
+          data_n <- as.numeric(c(data_n,t(data[nrow(data)-2,])[-1]))
+        }
+       
+      }
+      data_n[data_n<0] <- NA
+      # Read table and remove unnecessary columns
+ 
+      
+      cat(" - get station info \n")
+      m <- gregexpr('[0-9]+',basename(all_files[i]))
+      year = regmatches(basename(all_files[i]),m)[[1]]
+      
+ 
+      
+      # Rearrange table in two columns and sort by date
+      dates = seq(as.Date(paste0(year,"-01-01")),as.Date(paste0(year,"-12-31")),"days")
+      month_ok = format(dates,"%Y%m%d")
+      
+      if(st_name=="Chinda")  Q = 77.8574*( data_n - (-0.32))^ 1.8776
+      if(st_name=="ElTablon")  Q=94.059*(data_n-0.25)^1.9997
+      if(st_name=="LaVegona")  Q=24.609*(data_n+0.1116)^2.3236
+      if(st_name=="SF_Ojuera")  Q= 73.097*(data_n -0.090) ^ 2.1387
+      
+      
+      data_end <- cbind(month_ok, Q)
+      colnames(data_end) <- c("Date", "Value")
+      data_end <- data_end[order(data_end[,1]),]
+      
+      
+      oDirVar <- paste0(oDir,"/", "hdl-per-station")
+      if(!file.exists(oDirVar)){
+        dir.create(oDirVar, recursive = T)
+      }
+      
+        # write.table(data_end, paste0(oDirVar, "/tablon_raw_hdl.txt"), quote = F, col.names =F, row.names = F, append = T)
+        write.table(data_end, paste0(oDirVar,"/", st_name,"_raw_hdl.txt"), quote = F, col.names =F, row.names = F, append = T)
+        
+      
+      
+    }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+    
+    
+ # }
+  
+  
+  
+}
+
+### Run function
+lapply(1:length(xlsF_all),function(j) readDGRH_n(all_files = xlsF_all[[j]],st_name = st_name[j],iDir, xlsF, oDir))
+
 
