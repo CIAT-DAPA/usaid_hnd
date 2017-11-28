dcdd0328996784a224f13972e8b7dcae59511c9f
Jefferson Valencia Gómez <jevago262000@gmail.com>
2017-11-28 16:03:12 -0500

    fix of script to make maps of variables

--- 05_water_balance/07_plot_water_balance.R
+++ 05_water_balance/07_plot_water_balance.R
@@ -1,19 +1,19 @@
 ### Plot resulting variables of the water balance
+### Note: Do not use variable names that could be functions or masked variables of packages (e.g. "mask", "poly")
 ### Author: Jefferson Valencia Gomez
 ### email: j.valencia@cgiar.org, jefferson.valencia.gomez@gmail.com
 
 # Load libraries
-library(raster)
-library(rgdal)
-library(RColorBrewer)
-library(rasterVis)
-library(sp)
+require(raster)
+require(rgdal)
+require(maptools)
+require(rasterVis)
 
 # Network drive
 net_drive = "Y:"
 
 # Arguments
-iDir <- paste0(net_drive, "/Outputs/WPS/Balance_Hidrico/thornthwaite_and_mather")
+iDir <- paste0(net_drive, "/Outputs/WPS/Balance_Hidrico/thornthwaite_and_mather/final")
 varLs <- c("aet", "eprec", "perc", "runoff", "sstor", "bflow", "wyield")
 varRs <- c("Evapotranspiración Real", "Precipitacón Efectiva", "Percolación", "Escorrentía Superficial", "Humedad del Suelo", "Flujo Base", "Aporte de Agua")
 dicVar <- vector(mode="list", length=length(varLs))
@@ -23,27 +23,34 @@ for (i in 1:length(varLs)){
   dicVar[[i]] <- varRs[i]
 }
 
-oDir <- paste0(net_drive, "/Outputs/WPS/Balance_Hidrico/maps")
-mask <- paste0(net_drive, "/06_analysis/Extracts_MicroCuencas/mask/Microcuencas_ZOI_Finales.shp")
+oDir <- paste0(net_drive, "/Outputs/WPS/Balance_Hidrico/maps/final")
+mask_shp <- paste0(net_drive, "/06_analysis/Scenarios/masks/ZOI.shp")
+
+months = c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
 
 # read microwatersheds
-poly <- shapefile(mask)
+poly_shp <- readOGR(mask_shp, layer= "ZOI")
+#poly <- getData('GADM', country='Honduras', level=1)
 
 # define symbology of blues
 mytheme = rasterTheme(region = brewer.pal(9, "Blues"))
+mytheme$strip.border$col = "white"  # Eliminate frame from maps
+mytheme$axis.line$col = 'white'  # Eliminate frame from maps
 
 # For monthly timescale (average)
 for (var in varLs){
   
   # Raster stack
+  #rs_stk <- stack(paste0(iDir, "/", var, "/",  var, "_month_", 1:12, ".tif"))
   rs_stk <- stack(paste0(iDir, "/", var, "/",  var, "_month_", 1:12, ".tif"))
   
   cat(var, "\n")
 
   # create raster stack
-  rs_stk_crop <- crop(rs_stk, extent(poly))
-  extent(rs_stk_crop) <- extent(poly)
-  names(rs_stk_crop) = month.name
+  #rs_stk_crop = mask(crop(rs_stk, extent(poly)), poly)
+  rs_stk_crop <- crop(rs_stk, extent(poly_shp))
+  extent(rs_stk_crop) <- extent(poly_shp)
+  names(rs_stk_crop) = months
   #plot(rs_stk_crop)
   
   # plot title
@@ -55,11 +62,16 @@ for (var in varLs){
     file = image,
     width = 4000,
     height = 2800,
-    res = 300)
+    res = 300,
+    compression ='lzw')
 
   # scales=list(draw=FALSE) for no labels
-  print(levelplot(rs_stk_crop, scales=list(draw=FALSE), par.settings=mytheme, main=plot.title))
+  lvl.plot = levelplot(rs_stk_crop, scales=list(draw=FALSE), par.settings=mytheme, main=plot.title, colorkey = list(space = "bottom"))
+  
+  print(lvl.plot  + layer(sp.polygons(poly_shp)))
 
   dev.off()
-  
+ 
+  # Delete temp files
+  unlink(rasterOptions()$tmpdir, recursive=TRUE)
 }
