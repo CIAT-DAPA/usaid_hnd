4cbb6f369ac5402253a6e01b44913e211ea4bbae
Llanos Herrera <L.Llanos@cgiar.org>
2018-05-03 10:11:34 -0500

    Script to read sanaa stationes

--- 01_weather_stations/01b_read_wth_st_sanaa.R
+++ 01_weather_stations/01b_read_wth_st_sanaa.R
@@ -1,181 +1,130 @@
-## Authors: Lizeth Llanos l.llanos@cgiar.org / Carlos Navarro
+## Carlos Navarro
 ## CIAT-CCAFS
-## April 2018
+## February 2017
 ## USAID-HND
- 
+
 ## Needs java x-64 (if R is 64bits) https://www.java.com/en/download/manual.jsp 
-Sys.setenv(JAVA_HOME='C:/Program Files/Java/jre1.8.0_161')
+Sys.setenv(JAVA_HOME='C:/Program Files/Java/jre1.8.0_121')
 
-# Set libraries 
-options(java.parameters = "-Xmx8000m")
-library(rJava)
-# if(!require(XLConnect)){install.packages("XLConnect")}
-library(XLConnect)
-library(reshape)
+
+###### Convert meters to lat lon using projection for HND
+
+iDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_sanaa/monthly_raw/_primary_files"
+xlsF_all <- list.files(iDir, ".xls")
+oDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_sanaa/monthly_raw/prec-per-station"
+
+
+readDGRH <- function(iDir="", xlsF="", oDir=""){
+  
+  # Set libraries
+  # if(!require(XLConnect)){install.packages("XLConnect")}
+  library(XLConnect)
+  library(reshape)
+  
+  # Open xls file 
+ 
+  #nsheets <- length(getSheets(xlsF))
+  
+  st_catalog <- c()
+  
+  for (i in 1:length(xlsF_all)){
     
-#### Read COPECO-SERNA weather data ####
-
-var <- "tmin"
-# readCOPECO(iDir, xlsF, oDir, var)
-
-
-  if (var == "prec"){
+    #cat(".> Read Sheet", i , "/", nsheets, "\n")
+    xlsF <- loadWorkbook(paste0(iDir, "/", xlsF_all[i]))
+    xlsF_s <- readWorksheet(xlsF, sheet =1, header = FALSE, startRow = 4, startCol = 1, endCol = 13)
     
-    iDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw/_primary_files/Precipitacion Diaria"
-    xlsF <- "Precipitaciones.xlsx"
-    oDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw"
-
-    # Open xls file 
-    xlsF <- loadWorkbook(paste0(iDir, "/", xlsF))
-    nsheets <- length(getSheets(xlsF))
-    name_sh <- getSheets(xlsF)
-    st_catalog <- read.csv("Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw/_primary_files/coordenadas.csv",header = T)
+    # Read table and remove unnecessary columns
+    data <- xlsF_s[as.numeric(xlsF_s[,1]) > 1950,]
+    data <- data[!is.na(data[,1]),]
+    #if (ncol(data) >=15 && nrow(data) > 20){data <- data[, colSums(is.na(data)) != nrow(data)]}
+    #data <- data[,-(ncol(data))]
+    names(data) <- c("Year", 1:12)
     
-    for (i in 2:nsheets){
-      
-      cat(".> Read Sheet", i , "/", nsheets, "\n")
-      
-      xlsF_s <- readWorksheet(xlsF, sheet =i, header = FALSE, startRow = 7, startCol = 2, endCol = 34)
-      
-      # Read table and remove unnecessary columns
-      data <- xlsF_s[as.numeric(xlsF_s[,1]) >= 1970,]
-      data <- data[!is.na(data[,1]),]
-       names(data) <- c("Year", "Month", 1:31)
-      
-      # Rearrange table in two columns and sort by date
-      data <- melt(data,id=c("Year", "Month"))
-      names(data)[3] <- "Day"
-      data <- data [order(data$Year, data$Month),]
-      
-      dates_s <- as.Date(paste(data[,1], data[,2], data[,3], sep = "-"))
-      dates_f <- seq.Date(as.Date("1970-01-01"), as.Date(paste(data[nrow(data),1], 12, 31, sep="-")), "days")
-      
-      data_f <- data[which(dates_s %in% dates_f),]
-      
-      data_f <- cbind.data.frame(Date = paste(data_f[,1], sprintf("%02d", data_f[,2]),sprintf("%02d", data_f[,3]), sep=""), Value = as.numeric(as.character(data_f[,4])))
-      data_f <- data_f[order(data_f[,1]),]
-      
-      oDirVar <- paste0(oDir,"/", var, "-per-station")
-      if(!file.exists(oDirVar)){
-        dir.create(oDirVar, recursive = T)
-      }
-      
-      st_cod <- st_catalog[which(tolower(name_sh[i]) == tolower(st_catalog[,1])), 5]
-      cat(" - write whtst output file \n")
-      print(as.character(st_cod))
-      write.table(data_f, paste0(oDirVar, "/", st_cod, "_raw_", var, ".txt"), quote = F, row.names = F)
-      
-      
-    }
+    # Rearrange table in two columns and sort by date
+    data <- melt(data,id=c("Year"))
+    data <- cbind(paste(data[,1], sprintf("%02d", data[,2]), sep=""), as.numeric(as.character(data[,3])))
+    colnames(data) <- c("Date", "Value")
+    data <- data[order(data[,1]),]
     
+    cat(" - get station info \n")
+    
+    # Read station info
+    irow <- which(sapply(lapply(strsplit(xlsF_s[1:4,1], " "), function(ch) grep("estacion", tolower(ch))), function(x) length(x) > 0), arr.ind = TRUE)
+    irow <- strsplit(xlsF_s[irow, 1], " " )[[1]]
+    irow <- unlist(strsplit(irow[which(irow!="")],":"))
+    # irow <- gsub("estacion|:|departamento|tipo|lat|long|elev|msnm|codigo|subcuenca", "", tolower(irow))
+    # irow[which(irow == "")] <- NA
+    # irow <- irow[!is.na(irow)]
+     
+    st_name <- irow[grep("estacion", tolower(irow))+1]
+    if(irow[grep("estacion", tolower(irow))+4]!="")  st_name <-paste0(irow[grep("estacion", tolower(irow))+1],irow[grep("estacion", tolower(irow))+2])
    
     
-  } 
+      
+      st_lat <- irow[grep("lat", tolower(irow))+1]
+     # st_lat <- as.numeric(substr(st_lat[[1]][1], nchar(st_lat[[1]][1])-1, nchar(st_lat[[1]][1]))) + as.numeric(st_lat[[1]][2])/60 + as.numeric(st_lat[[1]][2])/3600
+      st_lon <- irow[grep("lat", tolower(irow))+1]
+    #  st_lon <- as.numeric(substr(st_lon[[1]][1], nchar(st_lon[[1]][1])-1, nchar(st_lon[[1]][1]))) + as.numeric(st_lon[[1]][2])/60 + as.numeric(st_lon[[1]][2])/3600
+      
+      st_cod <- irow[grep("codigo", tolower(irow))+1]
+      st_elv <- irow[grep("elev", tolower(irow))+1]
+
   
-  if (var == "tmax"){
-    
-    iDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw/_primary_files/Temperatura Máxima Absoluta"
-    xlsF_all <- list.files(iDir, ".xls")
-    oDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw"
-    
-    # Open xls file 
-    # nsheets <- length(getSheets(xlsF))
-    name_sh <- gsub(" ","",gsub(" Temperatura Máxima Absoluta.xls", "", xlsF_all))
-    st_catalog <- read.csv("Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw/_primary_files/coordenadas.csv",header = T)
-    
-    for (i in 1:length(xlsF_all)){
-      
-      cat(".> Read Sheet", i , "\n")
-      xlsF <- loadWorkbook(paste0(iDir, "/", xlsF_all[i]))
- 
-      xlsF_s <- readWorksheet(xlsF, sheet =1, header = FALSE, startRow = 7, startCol = 2, endCol = 34)
-      
-      # Read table and remove unnecessary columns
-      data <- xlsF_s[as.numeric(xlsF_s[,1]) >= 1970,]
-      data <- data[!is.na(data[,1]),]
-      names(data) <- c("Year", "Month", 1:31)
-      
-      # Rearrange table in two columns and sort by date
-      data <- melt(data,id=c("Year", "Month"))
-      names(data)[3] <- "Day"
-      data <- data [order(data$Year, data$Month),]
-      
-      dates_s <- as.Date(paste(data[,1], data[,2], data[,3], sep = "-"))
-      dates_f <- seq.Date(as.Date("1970-01-01"), as.Date(paste(data[nrow(data),1], 12, 31, sep="-")), "days")
-      
-      data_f <- data[which(dates_s %in% dates_f),]
-      
-      data_f <- cbind.data.frame(Date = paste(data_f[,1], sprintf("%02d", data_f[,2]),sprintf("%02d", data_f[,3]), sep=""), Value = as.numeric(as.character(data_f[,4])))
-      data_f <- data_f[order(data_f[,1]),]
-      
-      oDirVar <- paste0(oDir,"/", var, "-per-station")
-      if(!file.exists(oDirVar)){
-        dir.create(oDirVar, recursive = T)
-      }
-      
-      st_cod <- st_catalog[which(tolower(name_sh[i]) == tolower(st_catalog[,1])), 5]
-      cat(" - write whtst output file \n")
-      print(as.character(st_cod))
-      write.table(data_f, paste0(oDirVar, "/", st_cod, "_raw_", var, ".txt"), quote = F, row.names = F)
-      
-      
-    }
     
     
+    #irow <- which(sapply(lapply(strsplit(xlsF_s[,1], " "), function(ch) grep("cuenca", tolower(ch))), function(x) length(x) > 0), arr.ind = TRUE)
+    #irow <- xlsF_s[irow, ]
+    # irow <- gsub("cuenca|:| ", "", tolower(irow))
+    # irow[which(irow == "")] <- NA
+    # irow <- irow[!is.na(irow)]
+    # st_wth <- irow[1]
     
-  } 
+    # if(which(sapply(lapply(strsplit(xlsF_s[,1], " "), function(ch) grep("precipitacion", tolower(ch))), function(x) length(x) > 0), arr.ind = TRUE) > 0) {
+       var <- "prec"
+    # } 
+    
+    # oDirVar <- paste0(oDir, "/monthly-raw/", var, "-per-station")
+    # if(!file.exists(oDirVar)){
+    #   dir.create(oDirVar, recursive = T)
+    # }
+    
+    cat(" - write whtst output file \n")
+    write.table(data, paste0(oDir, "/", if(!is.na(as.numeric(st_cod))){as.numeric(st_cod)}else{st_name}, "_raw_", var, ".txt"), quote = F, row.names = F)
+    
+    st_catalog <- rbind(st_catalog, cbind(st_name, as.numeric(st_cod), var, st_lat, st_lon, st_elv, paste(data[1,1]), paste(data[nrow(data),1])))
+    rm(irow)
+    rm(data)
+  }
   
-  if (var == "tmin"){
-    
-    iDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw/_primary_files/Temperatura Mínima Absoluta/"
-    xlsF_all <- list.files(iDir, ".xls")
-    oDir <- "Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw"
-    
-    # Open xls file 
-    # nsheets <- length(getSheets(xlsF))
-    name_sh <- gsub(" ","",gsub(" Temperatura Mínima Absoluta.xls", "", xlsF_all))
-    st_catalog <- read.csv("Z:/Water_Planning_System/01_weather_stations/hnd_copeco/daily_raw/_primary_files/coordenadas.csv",header = T)
-    
-    for (i in 1:length(xlsF_all)){
-      
-      cat(".> Read Sheet", i , "\n")
-      xlsF <- loadWorkbook(paste0(iDir, "/", xlsF_all[i]))
-      
-      xlsF_s <- readWorksheet(xlsF, sheet =1, header = FALSE, startRow = 7, startCol = 2, endCol = 34)
-      
-      # Read table and remove unnecessary columns
-      data <- xlsF_s[as.numeric(xlsF_s[,1]) >= 1970,]
-      data <- data[!is.na(data[,1]),]
-      names(data) <- c("Year", "Month", 1:31)
-      
-      # Rearrange table in two columns and sort by date
-      data <- melt(data,id=c("Year", "Month"))
-      names(data)[3] <- "Day"
-      data <- data [order(data$Year, data$Month),]
-      
-      dates_s <- as.Date(paste(data[,1], data[,2], data[,3], sep = "-"))
-      dates_f <- seq.Date(as.Date("1970-01-01"), as.Date(paste(data[nrow(data),1], 12, 31, sep="-")), "days")
-      
-      data_f <- data[which(dates_s %in% dates_f),]
-      
-      data_f <- cbind.data.frame(Date = paste(data_f[,1], sprintf("%02d", data_f[,2]),sprintf("%02d", data_f[,3]), sep=""), Value = as.numeric(as.character(data_f[,4])))
-      data_f <- data_f[order(data_f[,1]),]
-      
-      oDirVar <- paste0(oDir,"/", var, "-per-station")
-      if(!file.exists(oDirVar)){
-        dir.create(oDirVar, recursive = T)
-      }
-      
-      st_cod <- st_catalog[which(tolower(name_sh[i]) == tolower(st_catalog[,1])), 5]
-      cat(" - write whtst output file \n")
-      print(as.character(st_cod))
-      write.table(data_f, paste0(oDirVar, "/", st_cod, "_raw_", var, ".txt"), quote = F, row.names = F)
-      
-      
-    }
-    
-    
-    
-  } 
+  cat(".> Write catalog file \n")
+  colnames(st_catalog) <- c("station", "code", "variable", "latitude", "longitude", "elevation", "start_date", "end_date")
+  write.csv(st_catalog, paste0(oDir, "/stations_catalog.csv"), quote = F, row.names = F)
   
   
+  
+}
+
+### Run function
+readDGRH(iDir, xlsF, oDir)
+
+
+#### Read DGRH weather data - qbasic data ####
+
+iDir <- "W:/01_weather_stations/hnd_dgrh/monthly_raw/_primary_files/qbasic"
+oDir <- "W:/01_weather_stations/hnd_dgrh/monthly_raw"
+
+stLs <- list.files(iDir)
+
+for(st in stLs){
+  st <- stLs[7]
+  if (file.size(paste0(iDir, "/", st)) > 3){
+    data <- readLines(paste0(iDir, "/", st))  
+    
+    irow <- which(sapply(lapply(strsplit(data, ":"), function(ch) grep("ESTACION", tolower(ch))), function(x) length(x) > 0), arr.ind = TRUE)
+    
+  }
+  
+  
+  
+  
+}
